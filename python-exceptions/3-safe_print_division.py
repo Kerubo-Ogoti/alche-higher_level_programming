b2b95e947d7c402716a94bc62f40ff0bbf55d973
Abdul-Jalil1234 <a.fuseini@alustudent.com>
2026-05-04 13:51:05 +0400

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a , b):
+def safe_print_division(a, b):
     try:
         result = a / b
-    except :
+    except 
         result = None
     finally :
-        print("Inside result:{}".format(result))
+        print("Inside result: {}".format(result))
     return result
 
